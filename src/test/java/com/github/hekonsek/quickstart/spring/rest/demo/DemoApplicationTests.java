89c7c9020348a42e5b3a34bad8c135f690896c0f
Henryk Konsek <hekonsek@gmail.com>
2023-07-04 17:46:22 +0200

    Initial commit.

---
+++ src/test/java/com/github/hekonsek/quickstart/spring/rest/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hekonsek.quickstart.spring.rest.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
